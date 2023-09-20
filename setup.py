28d9d65e09659634087f2a23341653fbe840ca7b
xiaoniaoyouhuajiang <2583473505@qq.com>
2023-09-20 13:58:13 +0800

    fix generated so file location

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools import Extension, setup, find_packages
 # prepare ext_modules
 ext_modules = [
     Extension(
-        "pargsort",["hpds/concurrent/argsort/pargsort.pyx"],
+        "hpds.concurrent.argsort.pargsort",["hpds/concurrent/argsort/pargsort.pyx"],
         language="c++", extra_compile_args=["-fopenmp", "-std=c++17"],
         extra_link_args=["-fopenmp"],include_dirs=[numpy.get_include()]
     ),
     Extension(
-        "cython_wrapper", ["hpds/wrapper/cython_wrapper.pyx"],
+        "hpds.wrapper.cython_wrapper", ["hpds/wrapper/cython_wrapper.pyx"],
         depends=['c_code.c'], include_dirs=[numpy.get_include()]
     )
 ]
@@ -30,8 +30,6 @@ class CythonCommand(build_ext):
 with open("README.md", "r", encoding="utf8") as rm:
     readme = rm.read()
 
-tem1 = cythonize(["hpds/concurrent/argsort/pargsort.pyx", "hpds/wrapper/cython_wrapper.pyx"])
-tem2 = cythonize(ext_modules, language_level="3")
 setup(
     name="hpds",
     version="0.0.1",
@@ -43,7 +41,7 @@ setup(
     long_description_content_type="text/markdown",
     author="xiaoniaoyouhuajiang",
     author_email="2583473505@qq.com",
-    cmdclass={"build_ext": build_ext},
+    # cmdclass={"build_ext": build_ext},
     license="MIT",
     classifiers=[
     "License :: OSI Approved :: MIT License",
@@ -51,5 +49,5 @@ setup(
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     ],
-    ext_modules=tem1,
+    ext_modules=cythonize(ext_modules, language_level="3")
 )
